4eecaf42ffd5e387d830851408d5d2cb9934b15d
shimpeiws <shimpeiws@gmail.com>
2018-07-15 22:23:00 +0900

    User camera

--- App.js
+++ App.js
@@ -10,65 +10,115 @@ import {
   Button,
   Alert,
   ScrollView,
-  FlatList
+  FlatList,
+  TouchableOpacity
 } from "react-native";
+import { Camera, Permissions } from "expo";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: "aaaaaaa" };
+    this.state = {
+      text: "aaaaaaa",
+      hasCameraPermission: null,
+      type: Camera.Constants.Type.back
+    };
   }
   async componentDidMount() {
     console.info("componentDidMount");
     const result = await axios.get("http://httpbin.org/");
     console.info("result", result);
+
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === "granted" });
   }
   render() {
+    const { hasCameraPermission } = this.state;
     const pic = {
       uri:
         "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
     };
-    return (
-      <View style={styles.container}>
-        {/* <ScrollView> */}
-        <FlatList
-          data={[
-            { key: "Devin" },
-            { key: "Jackson" },
-            { key: "James" },
-            { key: "Joel" },
-            { key: "John" },
-            { key: "Jillian" },
-            { key: "Jimmy" },
-            { key: "Julie" }
-          ]}
-          renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
-        />
-        <TextInput
-          style={{ height: 40 }}
-          placeholder="Type here !!!"
-          onChangeText={text => this.setState({ text })}
-        />
-        <Text>{this.state.text}</Text>
-        <Image source={pic} style={{ width: 193, height: 110 }} />
-        <Button
-          onPress={() => {
-            Alert.alert("Button Pressed");
-          }}
-          title="Press Me"
-        />
-      </View>
-      // </ScrollView>
-    );
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={styles.container}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: "transparent",
+                flexDirection: "row"
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: "flex-end",
+                  alignItems: "center"
+                }}
+                onPress={() => {
+                  this.setState({
+                    type:
+                      this.state.type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back
+                  });
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: "white" }}
+                >
+                  {" "}
+                  Flip{" "}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+          {/* <ScrollView> */}
+          <FlatList
+            data={[
+              { key: "Devin" },
+              { key: "Jackson" },
+              { key: "James" },
+              { key: "Joel" },
+              { key: "John" },
+              { key: "Jillian" },
+              { key: "Jimmy" },
+              { key: "Julie" }
+            ]}
+            renderItem={({ item }) => (
+              <Text style={styles.item}>{item.key}</Text>
+            )}
+          />
+          <TextInput
+            style={{ height: 40 }}
+            placeholder="Type here !!!"
+            onChangeText={text => this.setState({ text })}
+          />
+          <Text>{this.state.text}</Text>
+          <Image source={pic} style={{ width: 193, height: 110 }} />
+          <Button
+            onPress={() => {
+              Alert.alert("Button Pressed");
+            }}
+            title="Press Me"
+          />
+        </View>
+        // </ScrollView>
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    flex: 1
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center"
   },
   item: {
     padding: 10,
