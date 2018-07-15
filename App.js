9169b6d5bf2c4b2dd83ca4dd6b687bf406b1c2d9
shimpeiws <shimpeiws@gmail.com>
2018-07-15 20:29:08 +0900

    Implement

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image
+} from "react-native";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "aaaaaaa" };
+  }
+  render() {
+    const pic = {
+      uri:
+        "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
+    };
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here !!!"
+          onChangeText={text => this.setState({ text })}
+        />
+        <Text>{this.state.text}</Text>
+        <Image source={pic} style={{ width: 193, height: 110 }} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+AppRegistry.registerComponent("spike-react-native", () => App);
