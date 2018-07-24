ca5afa5a4798002a2a474765421409b648677e35
shimpeiws <shimpeiws@gmail.com>
2018-07-25 08:56:48 +0900

    Remove expo libs

--- App.js
+++ App.js
@@ -13,24 +13,18 @@ import {
   FlatList,
   TouchableOpacity
 } from "react-native";
-import { Camera, Permissions } from "expo";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: "aaaaaaa",
-      hasCameraPermission: null,
-      type: Camera.Constants.Type.back
+      text: "aaaaaaa"
     };
   }
   async componentDidMount() {
     console.info("componentDidMount");
     const result = await axios.get("http://httpbin.org/");
     console.info("result", result);
-
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === "granted" });
   }
   render() {
     const { hasCameraPermission } = this.state;
@@ -38,78 +32,63 @@ export default class App extends React.Component {
       uri:
         "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
     };
-    if (hasCameraPermission === null) {
-      return <View />;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <View style={styles.container}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: "transparent",
-                flexDirection: "row"
-              }}
-            >
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: "flex-end",
-                  alignItems: "center"
-                }}
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back
-                  });
-                }}
-              >
-                <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: "white" }}
-                >
-                  {" "}
-                  Flip{" "}
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-          {/* <ScrollView> */}
-          <FlatList
-            data={[
-              { key: "Devin" },
-              { key: "Jackson" },
-              { key: "James" },
-              { key: "Joel" },
-              { key: "John" },
-              { key: "Jillian" },
-              { key: "Jimmy" },
-              { key: "Julie" }
-            ]}
-            renderItem={({ item }) => (
-              <Text style={styles.item}>{item.key}</Text>
-            )}
-          />
-          <TextInput
-            style={{ height: 40 }}
-            placeholder="Type here !!!"
-            onChangeText={text => this.setState({ text })}
-          />
-          <Text>{this.state.text}</Text>
-          <Image source={pic} style={{ width: 193, height: 110 }} />
-          <Button
-            onPress={() => {
-              Alert.alert("Button Pressed");
+    return (
+      <View style={styles.container}>
+        {/* <Camera style={{ flex: 1 }} type={this.state.type}> */}
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "transparent",
+            flexDirection: "row"
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: "flex-end",
+              alignItems: "center"
             }}
-            title="Press Me"
-          />
+            onPress={() => {
+              Alert.alert("Touched");
+            }}
+          >
+            <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+              {" "}
+              Flip{" "}
+            </Text>
+          </TouchableOpacity>
         </View>
-        // </ScrollView>
-      );
-    }
+        {/* </Camera> */}
+        {/* <ScrollView> */}
+        <FlatList
+          data={[
+            { key: "Devin" },
+            { key: "Jackson" },
+            { key: "James" },
+            { key: "Joel" },
+            { key: "John" },
+            { key: "Jillian" },
+            { key: "Jimmy" },
+            { key: "Julie" }
+          ]}
+          renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+        />
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here !!!"
+          onChangeText={text => this.setState({ text })}
+        />
+        <Text>{this.state.text}</Text>
+        <Image source={pic} style={{ width: 193, height: 110 }} />
+        <Button
+          onPress={() => {
+            Alert.alert("Button Pressed");
+          }}
+          title="Press Me"
+        />
+      </View>
+      // </ScrollView>
+    );
   }
 }
 
